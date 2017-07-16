4f7da2cf83b8d8fce3d974f5a0cf81e277bf47d7
Mike Bild <mike.bild@googlemail.com>
2017-07-16 18:26:44 +0200

    Basic fetch example

--- basic-fetch-data/pages/Index.js
+++ basic-fetch-data/pages/Index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router'
 import styled from 'styled-components'
 
 const StyledList = styled.ul`
@@ -11,26 +10,23 @@ const StyledLink = styled.a`
   color: green;
 `
 
-export default class Index extends React.Component {
-  static async getInitialProps() {
-    const res = await fetch('https://api.github.com/users/codecommission/repos')
-    const data = await res.json()
+export const RepoList = props => (
+  <div>
+    <h1>Reunify GitHub Repositories</h1>
+    <StyledList>
+      {
+        props.repos &&
+        props.repos.map((x, i) => <li key={i}><StyledLink href={x.html_url} taget="_blank">{x.name}</StyledLink></li>)
+      }
+    </StyledList>
+  </div>
+)
 
-    return {
-      github: data.filter(x => x.name.indexOf('reunify') !== -1),
-    }
-  }
-
-  render() {
-    return (
-      <StyledList>
-        {
-          this.props.github &&
-          this.props.github.map((x, i) =>
-            <li key={i}><StyledLink href={x.html_url} taget="_blank">{x.name}</StyledLink></li>
-          )
-        }
-      </StyledList>
-    )
-  }
+RepoList.getInitialProps = async props => {
+  const res = await fetch('https://api.github.com/users/codecommission/repos')
+  const data = await res.json()
+  const repos = data.filter(x => x.name.indexOf('reunify') !== -1)
+  return {repos}
 }
+
+export default RepoList
